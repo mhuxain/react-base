889db1c7ffef01a6dce075d0fa5e1d32cf7ba8f1
huxain <h.xain@yahoo.com>
2016-10-15 08:13:11 +0700

    switched to yarn

--- src/app/App.js
+++ src/app/App.js
@@ -7,23 +7,21 @@ import Contracts from './contracts/index.jsx'
 import Collections from './infra/base/index.jsx'
 import Home from './Home'
 import NotFound from './NotFound'
+import {Navbar, NavItem} from 'react-materialize';
 
-import { Link as MLink, Navigation} from 'react-toolbox';
-// import Link as MLink from 'react-toolbox/lib/link';
 
-const actions = [
-  { label: 'Alarm', raised: true, icon: 'access_alarm'},
-  { label: 'Location', raised: true, accent: true, icon: 'room'}
-];
-
-const NavigationTest = () => (
-  <div>
-    <Navigation type='horizontal' actions={actions} />
-    <Navigation type='vertical'>
-      <MLink href='http://' label='Inbox' icon='inbox' />
-      <MLink href='http://' active label='Profile' icon='person' />
-    </Navigation>
-  </div>
+const Navigation = () => (
+  <Navbar brand='logo' left>
+    <li>
+        <Link to='/'>Home</Link>
+    </li>
+    <li>
+        <Link to='/contracts'>Contracts</Link>
+    </li>
+    <li><Link to='/contractors'>Contractors</Link></li>
+    
+    
+  </Navbar>
 );
 
 
@@ -36,13 +34,8 @@ export default class App extends React.Component {
     return (
       <Router>
         <div>
-          <NavigationTest />
-          <ul>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/contracts'>Contracts</Link></li>
-            <li><Link to='/contractors'>Contractors</Link></li>
-          </ul>
-          <hr />
+          
+          <Navigation />
           <Match exactly pattern='/' component={Home}/>
           <Match pattern='/contracts' component={Contracts}/>
           <Match pattern='/contractors' component={Contractors}/>
